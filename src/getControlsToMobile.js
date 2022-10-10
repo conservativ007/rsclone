2889150ed6ad347d60152c157cf3c22a7fb15fd5
Maks <alisa3d2007@gmail.com>
2022-10-10 11:32:10 +0300

    small changes to mobile controls

--- src/getControlsToMobile.js
+++ src/getControlsToMobile.js
@@ -1,29 +1,3 @@
-// import { player } from "./start_game";
-// let arrowUp = document.querySelector(".arrow-up");
-// let timerId = null;
-
-// export function getControlsToMobile() {
-//   arrowUp.addEventListener("mousedown", () => startJump(timerId));
-//   arrowUp.addEventListener("mouseup", () => stopJump(timerId));
-// }
-
-// function startJump() {
-//   timerId = setInterval(() => {
-//     player.y = player.y - 6;
-//   }, 1);
-// }
-
-// function stopJump(timerId) {
-//   clearInterval(timerId);
-//   console.log(player)
-// }
-
-// -------------------------------
-
-
-
-
-
 export function getControlsToMobile(player) {
   let bodyWidth = document.body.getBoundingClientRect().width;
   if (bodyWidth >= 700) return;
@@ -34,17 +8,14 @@ export function getControlsToMobile(player) {
   let controls = document.querySelector(".controls");
   let item = document.querySelector(".circle");
 
-  document.addEventListener("touchstart", handleTouchStart, false);
-  document.addEventListener("touchmove", handleTouchMove, false);
-  document.addEventListener("touchend", handleTouchEnd, false);
+  item.addEventListener("touchstart", handleTouchStart, false);
+  item.addEventListener("touchmove", handleTouchMove, false);
+  item.addEventListener("touchend", handleTouchEnd, false);
 
   let x1 = null;
   let y1 = null;
 
   document.querySelector(".controls").style.display = "block";
-
-
-
 
   function handleTouchStart(e) {
     let firstTouch = e.touches[0];
@@ -67,7 +38,34 @@ export function getControlsToMobile(player) {
     let diffY = y2 - containerY;
 
     moveCircle(diffX, diffY);
-    funcDiff(x2 - x1, y2 - y1);
+    prepareForTheMove(x2 - x1, y2 - y1);
+  }
+
+  function prepareForTheMove(x, y) {
+    if (x < -20 && (y <= 10 && y >= -20) && startMove === false) {
+      startMove = true;
+      moveToHero("move left");
+    }
+
+    else if (x < -20 && y <= -21 && startMove === false) {
+      startMove = true;
+      moveToHero("move left and up");
+    }
+
+    else if ((x >= -19 && x <= 19) && y < -29 && startMove === false) {
+      startMove = true;
+      moveToHero("move up");
+    }
+
+    else if ((x >= 20 && x <= 49) && y < -29 && startMove === false) {
+      startMove = true;
+      moveToHero("move up and right");
+    }
+
+    else if (x >= 30 && (y >= -20 && y <= 20) && startMove === false) {
+      startMove = true;
+      moveToHero("move right");
+    }
   }
 
   function moveCircle(x, y) {
@@ -75,48 +73,11 @@ export function getControlsToMobile(player) {
     item.style.top = y + "px";
   }
 
-  function funcDiff(xDiff, yDiff) {
-    console.log("x: ", Math.round(xDiff), "y: ", Math.round(yDiff));
-
-    // console.log("y: ", yDiff < -14 && xDiff < -14)
-
-    if (xDiff < 0 && (yDiff === 0 || yDiff <= -10) && startMove === false) {
-      startMove = true;
-      moveToHero("move left");
-      // console.log("move left");
-    }
-
-    else if (xDiff < -14 && yDiff < -14 && startMove === false) {
-      startMove = true;
-      moveToHero("move left and up");
-      // console.log("move left and up");
-    }
-
-    else if ((xDiff >= -15 && xDiff <= 0) && yDiff < -28 && startMove === false) {
-      startMove = true;
-      // console.log("move up");
-      moveToHero("move up");
-    }
-
-    else if (xDiff > 0 && (yDiff === 0 && yDiff >= - 10 && startMove === false)) {
-      console.log("move right");
-      startMove = true;
-      moveToHero("move right");
-    }
-
-    else if (xDiff > 0 && yDiff <= -30 && startMove === false) {
-      // console.log("move up and right");
-      startMove = true;
-      moveToHero("move up and right");
-    }
-  }
-
   function moveToHero(move) {
 
     if (move === "move up") {
       timerId = setInterval(() => {
-        // console.log("Timer ID: ", timerId);
-        player.y = player.y - 6;
+        player.y = player.y - 7;
       }, 1);
     }
 
@@ -129,25 +90,24 @@ export function getControlsToMobile(player) {
     if (move === "move left") {
       timerId = setInterval(() => {
         player.dx = player.dx -= 4;
+        player.sx = 100;
       }, 10);
     }
 
     if (move === "move up and right") {
       timerId = setInterval(() => {
         player.dx = player.dx += 2;
-        player.y = player.y - 6;
+        player.y = player.y - 7;
       }, 1);
     }
 
     if (move === "move left and up") {
       timerId = setInterval(() => {
         player.dx = player.dx -= 2;
-        player.y = player.y - 6;
+        player.y = player.y - 7;
+        player.sx = 200;
       }, 1);
     }
-
-    // console.log(player.jump())
-
   }
 
   function handleTouchEnd() {
@@ -156,9 +116,9 @@ export function getControlsToMobile(player) {
     let styleId = setTimeout(() => {
       item.style = ""
       clearInterval(styleId);
+      player.sx = 0;
     }, 200);
 
-    console.log("clearTimerID: ", timerId);
     clearInterval(timerId);
     startMove = false;
   }
